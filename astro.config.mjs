665fb8c20e306731726966589fb4b27b6e844db8
Tyler Rasmussen <blade409@live.com>
2024-02-15 02:47:36 -0600

    Updated Astro config
    Added new custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rasmatazz409.github.io',
+  site: 'https://rasmatazz.org',
   integrations: [tailwind()]
 });
